cdaf80213a8c3893da43330cee5bbfccb375a774
beatriznbt <btrznbt@gmail.com>
2022-07-10 13:49:40 -0300

    Qualquer coisa 3

--- pythonProject1/main.py
+++ pythonProject1/main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('flips')
+    print_hi('bia')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
